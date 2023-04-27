6a9bde23636829e835151a66414fef654ddd4e66
WaWaLwin <wwl@mit.com.mm>
2023-04-27 14:08:32 +0630

    combining admin system configuration module and api for store info configuration

--- StoreInfoConfiguration/registration.php
+++ StoreInfoConfiguration/registration.php
@@ -1,8 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
-\Magento\Framework\Component\ComponentRegistrar::MODULE,
-'MIT_StoreInfoConfiguration',
-__DIR__
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'MIT_StoreInfoConfiguration',
+    __DIR__
 );
-// StoreInfoConfiguration
-// MIT_StoreInfoConfiguration
