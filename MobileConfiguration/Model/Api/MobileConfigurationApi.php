f13d8b4eb94a3b8a8501c14d8bc131d53ac4aa3a
WaWaLwin <wwl@mit.com.mm>
2023-04-27 15:15:45 +0630

    changing api route and removing interface factory and logger

--- MobileConfiguration/Model/Api/MobileConfigurationApi.php
+++ MobileConfiguration/Model/Api/MobileConfigurationApi.php
@@ -1,26 +1,18 @@
 <?php
 namespace MIT\MobileConfiguration\Model\Api;
 
-use Psr\Log\LoggerInterface;
-
-use MIT\MobileConfiguration\Model\Api\MobileConfigurationApiFactory;
 use MIT\MobileConfiguration\Api\MobileConfigurationApiInterface;
 use MIT\MobileConfiguration\Helper\Data; 
 
 class MobileConfigurationApi implements MobileConfigurationApiInterface 
 { 
-    protected $logger;
-    protected $mobileConfigurationApiFactory;
     protected $dataHelper;
 
     public function __construct(
-        LoggerInterface $logger,
-        MobileConfigurationApiFactory $mobileConfigurationApiFactory,
+        
         Data $dataHelper
     )
-    {
-        $this->logger = $logger;
-        $this->mobileConfigurationApiFactory = $mobileConfigurationApiFactory;
+    {       
         $this->dataHelper = $dataHelper;
     }
     
@@ -37,13 +29,13 @@ class MobileConfigurationApi implements MobileConfigurationApiInterface
 
         if ($version_number <= $force_update_version ) {
             $response = ['status' => 1, 'message' => 'Version Need to be updated.'];
-            $this->logger->info('Version Need to be updated.');
+           
         } else if ($version_number <= $notification_version ) {
             $response = ['status' => 2, 'message' => 'Notification will be sent.'];
-            $this->logger->info('Notification will be sent.');
+           
         } else {
             $response = ['status' => 3, 'message' => 'It is up to date.'];
-            $this->logger->info('It is up to date.');
+            
         }
 
         return array($response); 
