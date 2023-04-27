e05eb506f48861a09ccbc298f96bbca95a14396c
WaWaLwin <wwl@mit.com.mm>
2023-04-27 14:10:48 +0630

    deleting StoreInfoConfigurationApi since it is already conbine with StoreInfo

--- StoreInfoConfigurationApi/registration.php
+++
@@ -1,6 +0,0 @@
-<?php
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'MIT_StoreInfoConfigurationApi',
-    __DIR__
-);
